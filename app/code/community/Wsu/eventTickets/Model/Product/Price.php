29626a9ae704967a2bb543305770333463947a19
Jeremy Bass <jeremybass@cableone.net>
2013-12-07 19:45:57 -0800

    revokes price model

--- app/code/community/Wsu/eventTickets/Model/Product/Price.php
+++ app/code/community/Wsu/eventTickets/Model/Product/Price.php
@@ -1,6 +1,4 @@
 <?php
-
-class Wsu_eventTickets_Model_Product_Price extends Mage_Catalog_Model_Product_Type_Price
-{
+class Wsu_eventTickets_Model_Product_Price extends Mage_Catalog_Model_Product_Type_Price {
 
 }
