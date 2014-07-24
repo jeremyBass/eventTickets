c70f83d1595dae1fd2932a1dec88ff04fe5be3d4
Jeremy Bass <jeremybass@cableone.net>
2014-07-23 22:51:07 -0700

    add an allow_guests attr

--- app/code/community/Wsu/eventTickets/sql/wsu_eventtickets_setup/install-1.0.1.php
+++ app/code/community/Wsu/eventTickets/sql/wsu_eventtickets_setup/install-1.0.1.php
@@ -254,7 +254,28 @@ if (!$installer->getAttributeId(Mage_Catalog_Model_Product::ENTITY, 'guest_limit
 	));
 }
  
- 
+if (!$installer->getAttributeId(Mage_Catalog_Model_Product::ENTITY, 'allow_guests')) {
+	$installer->addAttribute('catalog_product', 'allow_guests', array(
+			'input'                     => 'select',
+			'type'                      => 'int',
+			'label'                     => 'Allow Guests',
+			'source'                    => 'eav/entity_attribute_source_boolean',
+			'global'                    => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+			'visible'                   => 1,
+			'required'                  => 0,
+			'visible_on_front'          => 0,
+			'is_html_allowed_on_front'  => 0,
+			'is_configurable'           => 0,
+			'searchable'                => 0,
+			'filterable'                => 0,
+			'comparable'                => 0,
+			'unique'                    => false,
+			'user_defined'              => false,
+			'default'					=> '',
+			'is_user_defined'           => false,
+			'used_in_product_listing'   => true
+	));
+}
 
 
 
