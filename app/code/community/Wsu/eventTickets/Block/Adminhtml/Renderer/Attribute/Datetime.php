5b5882ea391f7308d08c5729d90110c0f1ff1179
Jeremy Bass <jeremybass@cableone.net>
2014-07-23 22:31:56 -0700

    adds a custom renderer for the datetime field

---
+++ app/code/community/Wsu/eventTickets/Block/Adminhtml/Renderer/Attribute/Datetime.php
@@ -0,0 +1,10 @@
+<?php
+
+class Wsu_eventTickets_Block_Adminhtml_Renderer_Attribute_Datetime extends Varien_Data_Form_Element_Date {
+	
+    public function getAfterElementHtml() {
+        $html = parent::getAfterElementHtml();
+        return "<div id='custom_block'>".$html."</div>";
+    }
+}
+
--- app/code/community/Wsu/eventTickets/sql/wsu_eventtickets_setup/install-1.0.1.php
+++ app/code/community/Wsu/eventTickets/sql/wsu_eventtickets_setup/install-1.0.1.php
@@ -13,7 +13,43 @@
 $installer = $this;
 $installer->startSetup();
 
+/*
 
+if (!$installer->getAttributeId(Mage_Catalog_Model_Product::ENTITY, 'attribute_name')) {
+    $installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'attribute_name', array(         // TABLE.COLUMN:                                       DESCRIPTION:
+        'label'                      => 'Label',                                                  // eav_attribute.frontend_label                        admin input label
+        'group'                      => 'General',                                                // (not a column)                                      tab in product edit screen
+        'sort_order'                 => 0  ,                                                       // eav_entity_attribute.sort_order                     sort order in group
+        'backend'                    => 'module/class_name',                                      // eav_attribute.backend_model                         backend class (module/class_name format)
+        'type'                       => 'varchar',                                                // eav_attribute.backend_type                          backend storage type (varchar, text etc)
+        'frontend'                   => 'module/class_name',                                      // eav_attribute.frontend_model                        admin class (module/class_name format)
+        'note'                       => null,                                                     // eav_attribute.note                                  admin input note (shows below input)
+        'default'                    => null,                                                     // eav_attribute.default_value                         admin input default value
+        'wysiwyg_enabled'            => false,                                                    // catalog_eav_attribute.is_wysiwyg_enabled            (products only) admin input wysiwyg enabled
+        'input'                      => 'input_name',                                             // eav_attribute.frontend_input                        admin input type (select, text, textarea etc)
+        'input_renderer'             => 'module/class_name',                                      // catalog_eav_attribute.frontend_input_renderer       (products only) admin input renderer (otherwise input is used to resolve renderer)
+        'source'                     => null,                                                     // eav_attribute.source_model                          admin input source model (for selects) (module/class_name format)
+        'required'                   => true,                                                     // eav_attribute.is_required                           required in admin
+        'user_defined'               => false,                                                    // eav_attribute.is_user_defined                       editable in admin attributes section, false for not
+        'unique'                     => false,                                                    // eav_attribute.is_unique                             unique value required
+        'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,  // catalog_eav_attribute.is_global                     (products only) scope
+        'visible'                    => true,                                                     // catalog_eav_attribute.is_visible                    (products only) visible on admin
+        'visible_on_front'           => false,                                                    // catalog_eav_attribute.is_visible_on_front           (products only) visible on frontend (store) attribute table
+        'used_in_product_listing'    => false,                                                    // catalog_eav_attribute.used_in_product_listing       (products only) made available in product listing
+        'searchable'                 => false,                                                    // catalog_eav_attribute.is_searchable                 (products only) searchable via basic search
+        'visible_in_advanced_search' => false,                                                    // catalog_eav_attribute.is_visible_in_advanced_search (products only) searchable via advanced search
+        'filterable'                 => false,                                                    // catalog_eav_attribute.is_filterable                 (products only) use in layered nav
+        'filterable_in_search'       => false,                                                    // catalog_eav_attribute.is_filterable_in_search       (products only) use in search results layered nav
+        'comparable'                 => false,                                                    // catalog_eav_attribute.is_comparable                 (products only) comparable on frontend
+        'is_html_allowed_on_front'   => true,                                                     // catalog_eav_attribute.is_visible_on_front           (products only) seems obvious, but also see visible
+        'apply_to'                   => 'simple,configurable',                                    // catalog_eav_attribute.apply_to                      (products only) which product types to apply to
+        'is_configurable'            => false,                                                    // catalog_eav_attribute.is_configurable               (products only) used for configurable products or not
+        'used_for_sort_by'           => false,                                                    // catalog_eav_attribute.used_for_sort_by              (products only) available in the 'sort by' menu
+        'position'                   => 0,                                                        // catalog_eav_attribute.position                      (products only) position in layered naviagtion
+        'used_for_promo_rules'       => false,                                                    // catalog_eav_attribute.is_used_for_promo_rules       (products only) available for use in promo rules
+    ));
+}
+*/
 
 
 
@@ -152,32 +188,37 @@ $defaultAttrSetId = Mage::getModel('catalog/product')->getDefaultAttributeSetId(
 	);
 
 
- $installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'event_start_date_time', array(
-     'input'         => 'datetime',
-     'type'          => 'datetime',
-     'time'          => true,
-     'label'         => 'Date & Time',
-     'visible'       => true,
-     'required'      => false,
-     'user_defined'  => true,
-     'visible_on_front' => true,
-     'backend'       => 'eav/entity_attribute_backend_time_created',
-     'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL
- ));
+if (!$installer->getAttributeId(Mage_Catalog_Model_Product::ENTITY, 'event_start_date_time')) {
+	 $installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'event_start_date_time', array(
+		 'input'         => 'datetime',
+		 'type'          => 'datetime',
+		 'time'          => true,
+		 'label'         => 'Date & Time',
+		 'input_renderer'=> 'wsu_eventtickets/adminhtml_renderer_attribute_datetime',
+		 'visible'       => true,
+		 'required'      => false,
+		 'user_defined'  => true,
+		 'visible_on_front' => true,
+		 'backend'       => 'eav/entity_attribute_backend_time_created',
+		 'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL
+	 ));
+}
 
-  $installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'event_end_date_time', array(
-     'input'         => 'datetime',
-     'type'          => 'datetime',
-     'time'          => true,
-     'label'         => 'Date & Time',
-     'visible'       => true,
-     'required'      => false,
-     'user_defined'  => true,
-     'visible_on_front' => true,
-     'backend'       => 'eav/entity_attribute_backend_time_created',
-     'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL
- ));
-
+if (!$installer->getAttributeId(Mage_Catalog_Model_Product::ENTITY, 'event_end_date_time')) {
+	$installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'event_end_date_time', array(
+		'input'         => 'datetime',
+		'type'          => 'datetime',
+		'time'          => true,
+		'label'         => 'Date & Time',
+		'input_renderer'=> 'wsu_eventtickets/adminhtml_renderer_attribute_datetime',
+		'visible'       => true,
+		'required'      => false,
+		'user_defined'  => true,
+		'visible_on_front' => true,
+		'backend'       => 'eav/entity_attribute_backend_time_created',
+		'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL
+	));
+}
  
  
  
