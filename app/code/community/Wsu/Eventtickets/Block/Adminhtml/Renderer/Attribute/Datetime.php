17106c37e25206b7096e8eb23a2b9631133e75e7
jeremyBass <jeremybass@cableone.net>
2014-07-30 10:56:42 -0700

    wrong lable

--- app/code/community/Wsu/Eventtickets/Block/Adminhtml/Renderer/Attribute/Datetime.php
+++ app/code/community/Wsu/Eventtickets/Block/Adminhtml/Renderer/Attribute/Datetime.php
@@ -2,9 +2,9 @@
 
 class Wsu_Eventtickets_Block_Adminhtml_Renderer_Attribute_Datetime extends Varien_Data_Form_Element_Date {
 	
-    public function getAfterElementHtml() {
+    /*public function getAfterElementHtml() {
         $html = parent::getAfterElementHtml();
         return "<div id='custom_block'>".$html."</div>";
-    }
+    }*/
 }
 
