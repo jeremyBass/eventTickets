1444ba7bfd9e7c517815f022c48b22f443d6979f
Jeremy Bass <jeremybass@cableone.net>
2015-04-17 12:02:35 -0700

    forgot to do the clean up on the renderer

--- app/code/community/Wsu/Eventtickets/Block/Adminhtml/Eventtickets/Registrants/Renderer/Preview.php
+++ app/code/community/Wsu/Eventtickets/Block/Adminhtml/Eventtickets/Registrants/Renderer/Preview.php
@@ -4,23 +4,9 @@ class Wsu_Eventtickets_Block_Adminhtml_Eventtickets_Registrants_Renderer_Preview
 
     public function render(Varien_Object $row) {
 		$product_id = $row->getData('entity_id');
-		/*$product_collection = Mage::getModel('catalog/product')->getCollection()
-			->addAttributeToFilter('entity_id', $id)
-			->addUrlRewrite();
-		$href = $product_collection->getFirstItem()->getProductUrl();*/
 		$_product = Mage::getModel('catalog/product')->load($product_id); 
  		$href = $_product->getProductUrl();
-		//$href = Mage::getResourceSingleton('catalog/product')->getAttributeRawValue($id, 'url_key', Mage::app()->getStore());
-		
-		
-		//$href = Mage::helper('adminhtml')->getUrl('adminhtml/catalog_product/edit/') .'id/$entity_id';
-    	$html = '<a href="'.$href.'" target="_blank"><i class="fa fa-eye"></i>View</a>';
-	   
-	   
-	   
-//	   'caption' => Mage::helper('wsu_eventtickets')->__('View'),//
-//                    'url'     => array('base' => '*/*/registrants'),
-//                    'field'   => 'id'
+    	$html = '<a href="'.$href.'" target="_blank"><i class="fa fa-eye"></i> View</a>';
         return $html;
     }
 
